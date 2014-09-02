306d1412a0f3c946d3a981b47cccb365e9ecdd3c
Alejandro <agpituk@gmail.com>
2014-09-02 11:11:59 +0100

    Updated the dump.sql

--- htdocs/app/db/dump.sql
+++ htdocs/app/db/dump.sql
@@ -18,6 +18,54 @@ USE `wargames`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `battle`
+--
+
+DROP TABLE IF EXISTS `battle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `battle` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_prov_status` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `battle`
+--
+
+LOCK TABLES `battle` WRITE;
+/*!40000 ALTER TABLE `battle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `battle` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `battle_participants`
+--
+
+DROP TABLE IF EXISTS `battle_participants`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `battle_participants` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `beginning_points` int(11) NOT NULL,
+  `ending_points` int(11) DEFAULT NULL,
+  `result` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `battle_participants`
+--
+
+LOCK TABLES `battle_participants` WRITE;
+/*!40000 ALTER TABLE `battle_participants` DISABLE KEYS */;
+/*!40000 ALTER TABLE `battle_participants` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `campaign`
 --
 
@@ -39,6 +87,226 @@ LOCK TABLES `campaign` WRITE;
 /*!40000 ALTER TABLE `campaign` DISABLE KEYS */;
 INSERT INTO `campaign` VALUES (1,'First campaign!');
 /*!40000 ALTER TABLE `campaign` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `connection`
+--
+
+DROP TABLE IF EXISTS `connection`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `connection` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `from_province` int(11) NOT NULL,
+  `to_province` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `connection`
+--
+
+LOCK TABLES `connection` WRITE;
+/*!40000 ALTER TABLE `connection` DISABLE KEYS */;
+/*!40000 ALTER TABLE `connection` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order`
+--
+
+DROP TABLE IF EXISTS `order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_turn` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order`
+--
+
+LOCK TABLES `order` WRITE;
+/*!40000 ALTER TABLE `order` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `player`
+--
+
+DROP TABLE IF EXISTS `player`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `player` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_campaign` int(11) NOT NULL,
+  `resources` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `player`
+--
+
+LOCK TABLES `player` WRITE;
+/*!40000 ALTER TABLE `player` DISABLE KEYS */;
+/*!40000 ALTER TABLE `player` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `province`
+--
+
+DROP TABLE IF EXISTS `province`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `province` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `number` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `province`
+--
+
+LOCK TABLES `province` WRITE;
+/*!40000 ALTER TABLE `province` DISABLE KEYS */;
+/*!40000 ALTER TABLE `province` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `province_status`
+--
+
+DROP TABLE IF EXISTS `province_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `province_status` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_province` int(11) NOT NULL,
+  `id_player` int(11) NOT NULL,
+  `id_turn` int(11) NOT NULL,
+  `points` int(11) NOT NULL DEFAULT '0',
+  `id_random` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `province_status`
+--
+
+LOCK TABLES `province_status` WRITE;
+/*!40000 ALTER TABLE `province_status` DISABLE KEYS */;
+/*!40000 ALTER TABLE `province_status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `random`
+--
+
+DROP TABLE IF EXISTS `random`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `random` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_prov_status` int(11) NOT NULL,
+  `id_random_type` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `random`
+--
+
+LOCK TABLES `random` WRITE;
+/*!40000 ALTER TABLE `random` DISABLE KEYS */;
+/*!40000 ALTER TABLE `random` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `random_type`
+--
+
+DROP TABLE IF EXISTS `random_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `random_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(512) DEFAULT NULL,
+  `function_path` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `random_type`
+--
+
+LOCK TABLES `random_type` WRITE;
+/*!40000 ALTER TABLE `random_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `random_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `resource`
+--
+
+DROP TABLE IF EXISTS `resource`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `resource` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_prov_status` int(11) NOT NULL,
+  `points` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `resource`
+--
+
+LOCK TABLES `resource` WRITE;
+/*!40000 ALTER TABLE `resource` DISABLE KEYS */;
+/*!40000 ALTER TABLE `resource` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `result_type`
+--
+
+DROP TABLE IF EXISTS `result_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `result_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `action` varchar(45) DEFAULT NULL,
+  `percentage` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `result_type`
+--
+
+LOCK TABLES `result_type` WRITE;
+/*!40000 ALTER TABLE `result_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `result_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -69,30 +337,6 @@ LOCK TABLES `user` WRITE;
 INSERT INTO `user` VALUES (1,'agpituk@gmail.com','$2a$08$F0kk5/0uU.MrwIwxiVTpF.hmNb4bW/P4Wm0Mnp5CRRnOH6xZPtMU.','Alejandro Gonzalez','god'),(2,'hawernawer@gmail.com','$2a$08$gzskc1oRL6oPx9t20pArQ.ggLzPZ14Oges48XHg2z0F1k3tpYDaBC','hawernawer','user');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `users_campaigns`
---
-
-DROP TABLE IF EXISTS `users_campaigns`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users_campaigns` (
-  `id_user` int(11) NOT NULL,
-  `id_campaign` varchar(45) NOT NULL,
-  PRIMARY KEY (`id_user`,`id_campaign`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users_campaigns`
---
-
-LOCK TABLES `users_campaigns` WRITE;
-/*!40000 ALTER TABLE `users_campaigns` DISABLE KEYS */;
-INSERT INTO `users_campaigns` VALUES (1,'1');
-/*!40000 ALTER TABLE `users_campaigns` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -103,4 +347,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-08-22 15:00:21
+-- Dump completed on 2014-09-02 11:11:42
